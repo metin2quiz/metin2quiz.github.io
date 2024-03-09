44863e8a1c1d2ad191d4630a95d60dfc7968c05f
Emirhan Uzuner <emirhannuz@gmail.com>
2024-03-09 14:09:27 +0300

    initial

---
+++ index.js
@@ -0,0 +1,430 @@
+const items = {
+    "silahlar": [
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 1",
+            "itemName": "Kılıç",
+            "itemImage": "/images/b/bd/K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 5",
+            "itemName": "Uzun Kılıç",
+            "itemImage": "/images/3/39/Uzun_K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 10",
+            "itemName": "Hilal Kılıç",
+            "itemImage": "/images/f/f1/Hilal_K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 15",
+            "itemName": "Bambu Kılıcı",
+            "itemImage": "/images/5/57/Bambu_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 20",
+            "itemName": "Geniş Kılıç",
+            "itemImage": "/images/e/ec/Geni%C5%9F_K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 25",
+            "itemName": "Gümüş Kılıç",
+            "itemImage": "/images/d/df/G%C3%BCm%C3%BC%C5%9F_K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "2"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 30",
+            "itemName": "Dolunay Kılıcı",
+            "itemImage": "/images/7/7f/Dolunay_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "2"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 32",
+            "itemName": "Orkide Kılıcı",
+            "itemImage": "/images/7/7c/Orkide_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "2"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 36",
+            "itemName": "Sahte Kılıç",
+            "itemImage": "/images/9/91/Sahte_K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "2"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 40",
+            "itemName": "Barbar Kılıcı",
+            "itemImage": "/images/5/5a/Barbar_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "2"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 45",
+            "itemName": "Kanlı Kılıç",
+            "itemImage": "/images/1/1a/Kanl%C4%B1_K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 50",
+            "itemName": "Büyük Kılıç",
+            "itemImage": "/images/3/3b/B%C3%BCy%C3%BCk_K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 55",
+            "itemName": "Uçan Büyücü Kılıcı",
+            "itemImage": "/images/e/e1/U%C3%A7an_B%C3%BCy%C3%BCc%C3%BC_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 60",
+            "itemName": "Yarımay Kılıcı",
+            "itemImage": "/images/c/c1/Yar%C4%B1may_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 65",
+            "itemName": "Muharebe Kılıcı",
+            "itemImage": "/images/2/23/Muharebe_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 65",
+            "itemName": "Su Perisi Kılıcı",
+            "itemImage": "/images/c/c1/Su_Perisi_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 70",
+            "itemName": "Azap Kılıcı",
+            "itemImage": "/images/3/33/Azap_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 75",
+            "itemName": "Zehir Kılıcı",
+            "itemImage": "/images/c/c4/Zehir_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 80-90",
+            "itemName": "Triton Kılıcı",
+            "itemImage": "/images/5/5f/Triton_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 87-105",
+            "itemName": "Runik Kılıcı",
+            "itemImage": "/images/3/3f/Runik_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 96",
+            "itemName": "Kara Ejderha Kılıcı",
+            "itemImage": "/images/1/17/Kara_Ejderha_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 105",
+            "itemName": "Zodyak Kılıç",
+            "itemImage": "/images/4/46/Zodyak_K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 120",
+            "itemName": "Yılan Kılıcı",
+            "itemImage": "/images/b/b6/Y%C4%B1lan_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 1",
+            "itemName": "Geniş Mızrak",
+            "itemImage": "/images/e/e2/Geni%C5%9F_M%C4%B1zrak.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 5",
+            "itemName": "Mızrak",
+            "itemImage": "/images/b/bb/M%C4%B1zrak.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 10",
+            "itemName": "Giyotin Pala",
+            "itemImage": "/images/a/a7/Giyotin_Pala.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 15",
+            "itemName": "Örümcek Mızrağı",
+            "itemImage": "/images/2/23/%C3%96r%C3%BCmcek_M%C4%B1zra%C4%9F%C4%B1.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 20",
+            "itemName": "Kargı",
+            "itemImage": "/images/7/7f/Karg%C4%B1.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 25",
+            "itemName": "Savaş Tırpanı",
+            "itemImage": "/images/2/2e/Sava%C5%9F_T%C4%B1rpan%C4%B1.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 30",
+            "itemName": "Kırmızı Demir Pala",
+            "itemImage": "/images/a/ac/K%C4%B1rm%C4%B1z%C4%B1_Demir_Pala.png",
+            "tasSlotu": "1"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 32",
+            "itemName": "Askeri Tırmık",
+            "itemImage": "/images/9/97/Askeri_T%C4%B1rm%C4%B1k.png",
+            "tasSlotu": "2"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 36",
+            "itemName": "Baltalı Mızrak",
+            "itemImage": "/images/e/ef/Baltal%C4%B1_M%C4%B1zrak.png",
+            "tasSlotu": "2"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 40",
+            "itemName": "Büyük Balta",
+            "itemImage": "/images/e/ea/B%C3%BCy%C3%BCk_Balta.png",
+            "tasSlotu": "2"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 45",
+            "itemName": "Buzlu Uç",
+            "itemImage": "/images/c/c6/Buzlu_U%C3%A7.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 50",
+            "itemName": "Oniki Ruh Kılıcı",
+            "itemImage": "/images/f/f0/Oniki_Ruh_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 55",
+            "itemName": "Kurtuluş Palası",
+            "itemImage": "/images/0/03/Kurtulu%C5%9F_Palas%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 60",
+            "itemName": "Aslan Katleden",
+            "itemImage": "/images/0/09/Aslan_Katleden.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 65",
+            "itemName": "Partizan",
+            "itemImage": "/images/c/c6/Partizan.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 65",
+            "itemName": "Elektromanyetik Pala",
+            "itemImage": "/images/e/ee/Elektromanyetik_Pala.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 70",
+            "itemName": "Yarı İnsan Kılıç",
+            "itemImage": "/images/6/6c/Yar%C4%B1_%C4%B0nsan_K%C4%B1l%C4%B1%C3%A7.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 75",
+            "itemName": "Kin Kılıcı",
+            "itemImage": "/images/9/9f/Kin_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 87-105",
+            "itemName": "Güneş Kılıcı",
+            "itemImage": "/images/a/a2/G%C3%BCne%C5%9F_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 96",
+            "itemName": "Kara Ejderha Bıçağı",
+            "itemImage": "/images/9/94/Kara_Ejderha_B%C4%B1%C3%A7a%C4%9F%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 105",
+            "itemName": "Zodyak Orağı",
+            "itemImage": "/images/8/8d/Zodyak_Ora%C4%9F%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 105-115",
+            "itemName": "Kyanit Bıçağı",
+            "itemImage": "/images/8/8a/Kyanit_B%C4%B1%C3%A7a%C4%9F%C4%B1.png",
+            "tasSlotu": "3"
+        },
+        {
+            "kategori": "Kılıçlar",
+            "level": "Seviye 120",
+            "itemName": "Yılan Büyük Kılıcı",
+            "itemImage": "/images/0/02/Y%C4%B1lan_B%C3%BCy%C3%BCk_K%C4%B1l%C4%B1c%C4%B1.png",
+            "tasSlotu": "3"
+        }
+    ]
+
+};
+
+const clearScoreElement = document.getElementById('clear-score'),
+    correctElement = document.getElementById('correct'),
+    wrongElement = document.getElementById('wrong'),
+    answersDivElement = document.getElementById('answers'),
+    answerButtonElements = document.getElementsByClassName('answer-button'),
+    itemImageElement = document.getElementById('item-image');
+
+
+const getRandomCategoryItems = (data) => {
+    const keys = Object.keys(data);
+    const randomCategoryIndex = getRandomInt(keys.length - 1);
+    const randomCategoryName = keys[randomCategoryIndex];
+    const randomCategory = items[randomCategoryName];
+    return randomCategory;
+};
+
+const newQuestion = () => {
+    const randomCategoryItems = getRandomCategoryItems(items),
+        randomCategoryItemsLength = randomCategoryItems.length;
+
+    const answers = [],
+        generatedIndexes = [];
+    for (let index = 0; index < 4; index++) {
+        let randomItemIndex = getRandomInt(randomCategoryItemsLength - 1, generatedIndexes);
+        generatedIndexes.push(randomItemIndex);
+        let randomItem = randomCategoryItems[randomItemIndex];
+        answers.push(randomItem);
+    }
+
+    const correctAnswerIndex = getRandomInt(3);
+    const correctItem = answers[correctAnswerIndex];
+    const imageUrl = `https://tr-wiki.metin2.gameforge.com${correctItem.itemImage}`;
+    itemImageElement.setAttribute('src', imageUrl);
+    generateAnswers(answers, correctAnswerIndex);
+};
+
+
+const clearScore = () => {
+    correctElement.innerText = 0;
+    wrongElement.innerText = 0;
+};
+
+const generateAnswers = (answers = [], correctIndex = 0) => {
+    answersDivElement.innerHTML = '';
+    answers.forEach((answer, index) => {
+        let correct = index == correctIndex ? 1 : 0;
+        const buttonElement = document.createElement('button');
+        buttonElement.type = 'button';
+        buttonElement.className = 'btn btn-secondary me-2 answer-button';
+        buttonElement.innerText = answer.itemName;
+        buttonElement.setAttribute('data-cor', correct);
+        buttonElement.addEventListener('click', checkAnswer);
+        // let btn = `<button type="button" class="btn btn-secondary me-2 answer-button" data-cor="${correct}">${answer.itemName}</button>`;
+        answersDivElement.appendChild(buttonElement);
+    })
+
+}
+
+
+function getRandomInt(max, exclude = []) {
+    let randomNumber = Math.floor(Math.random() * max);
+    while (exclude.includes(randomNumber)) {
+        randomNumber = Math.floor(Math.random() * max);
+    }
+
+    return randomNumber;
+}
+
+
+function checkAnswer(element) {
+    const isCorrect = element.target.getAttribute('data-cor');
+    element = element.target;
+    if (isCorrect == '1') {
+        let current = correctElement.innerHTML;
+        correctElement.innerHTML = Number(current) + 1;
+        element.classList.remove('btn-secondary');
+        element.classList.add('btn-success');
+    }
+    else {
+        let current = wrongElement.innerHTML;
+        wrongElement.innerHTML = Number(current) + 1;
+        element.classList.remove('btn-secondary');
+        element.classList.add('btn-warning');
+    }
+    setTimeout(() => {
+        newQuestion();
+    }, 1200)
+
+}
+
+clearScoreElement.addEventListener('click', clearScore);
+
+// answerButtonElements.forEach(element => {
+
+// });
+
+for (let index = 0; index < answerButtonElements.length; index++) {
+    const element = answerButtonElements[index];
+    element.addEventListener('click', checkAnswer);
+}
+
+newQuestion();
